4fa2f61c31e856f2a3b42f5c73e299122c1544a4
hisoka176 <hisoka176@sina.com>
2017-04-10 16:00:11 +0000

    jdata_action

--- src/main/scala/Action.scala
+++ src/main/scala/Action.scala
@@ -10,6 +10,23 @@ class Action(logFile:String){
 	  import spark.implicits._
 	  println(this.logFile)
 	  val data = spark.read.option("header","true").option("encoding","gbk").csv(this.logFile);
+	  data.createOrReplaceTempView("jdata_action")
+	  val sqlDF = spark.sql("select * from jdata_action limit 10")
+	  sqlDF.show()
+	  val typeDF = spark.sql("select distinct(type) from jdata_action");
+	  typeDF.show()
+	  sqlString = "
+	  	    select user_id,sku_id,
+		    	   if(type='1',1,0) as type1,
+			   if(type='2',2,0) as type2,
+			   if(type='3',3,0) as type3,
+			   if(type='4',4,0) as type4,
+			   if(type='5',5,0) as type5,
+			   if(type='6',6,0) as type6,
+		    from jdata_action
+		    
+	  println("haha")
+	  data.printSchema()
 	  data.show()
 	  }
 }
